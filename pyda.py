712b59e27c31172941cc57146fbf9b1f5752a2f1
ommeleven <dangeom94@gmail.com>
2021-09-30 17:11:45 +0530

    Add files via upload
    Built a virtual assistant, using WolframAlpha and Wikipedia APIs. Only works through the console.  GUI to be added later.

---
+++ pyda.py
@@ -0,0 +1,10 @@
+import wx
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None,
+            pos = wx.DefaultPoition, size = wx.Size(450, 100),
+            style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
+            wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+            title = "PyDa")
+        
